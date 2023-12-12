8f42e28f055f50e6d474656365e0f8da9f74e0be
rodri-dot-git <rodri23091998@gmail.com>
2023-12-12 14:37:03 -0600

    Initial Commit

---
+++ minesweeperui/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  minesweeperui
+//
+//  Created by Rodrigo de Leon Castilla on 12/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ minesweeperui/minesweeperuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  minesweeperuiApp.swift
+//  minesweeperui
+//
+//  Created by Rodrigo de Leon Castilla on 12/12/23.
+//
+
+import SwiftUI
+
+@main
+struct minesweeperuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
